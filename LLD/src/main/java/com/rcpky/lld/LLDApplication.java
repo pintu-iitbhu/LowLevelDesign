acb60a70cf4b0522dece3a90cb41be21bddc8c6c
pintu_yadav <pkyadav@baraq.com.sa>
2025-08-01 16:39:55 +0530

    Initial commit

---
+++ LLD/src/main/java/com/rcpky/lld/LLDApplication.java
@@ -0,0 +1,13 @@
+package com.rcpky.lld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LLDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LLDApplication.class, args);
+	}
+
+}
---
+++ LLD/src/test/java/com/rcpky/lld/LowLevelDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcpky.lld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowLevelDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
